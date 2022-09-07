9ebc2dd913759ae787a6fcb481e17c45ae223ea6
Mauro Scheltens <mauroscheltens@hotmail.com>
2022-09-07 11:25:59 +0200

    fix: login-and-register-functionality

--- registreer.php
+++ registreer.php
@@ -8,7 +8,7 @@ session_start();
 
 <head>
   <script src="https://cdn.tailwindcss.com"></script>
-  <title>BitOverflow | Login</title>
+  <title>BitOverflow | Registreer</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </head>
 
@@ -27,40 +27,40 @@ session_start();
         </div>
       </div>
 
-      <form action="post" class="flex flex-col items-center mt-10">
+      <form method='POST' class="flex flex-col items-center mt-10">
         <div class="grid gap-4 lg:grid-cols-2 ">
           <div style="justify-content: space-between;">
             <p style="font-family: Laro;" class="ml-2 mb-2 text-white">VOORNAAM:</p>
-            <input type="text" class="rounded-full lg:px-3 py-2 pl-4 text-white outline-none ml-1" style="background-color: #3D3D3D; font-family: Poppins;" placeholder="VOORNAAM">
+            <input type="text" name='first_name' class="rounded-full lg:px-3 py-2 pl-4 text-white outline-none ml-1" style="background-color: #3D3D3D; font-family: Poppins;" placeholder="VOORNAAM">
           </div>
           <div>
             <p style="font-family: Laro;" class="ml-2 mb-2 text-white">ACHTERNAAM:</p>
-            <input type="text" class="rounded-full lg:px-3 px-1 py-2 pl-4 text-white outline-none" style="background-color: #3D3D3D; font-family: Poppins;" placeholder="ACHTERNAAM">
+            <input type="text" name='last_name' class="rounded-full lg:px-3 px-1 py-2 pl-4 text-white outline-none" style="background-color: #3D3D3D; font-family: Poppins;" placeholder="ACHTERNAAM">
           </div>
           <div>
             <p style="font-family: Laro;" class="ml-2 mb-2 text-white">E-MAIL:</p>
-            <input type="email" class="rounded-full lg:px-3 px-1 py-2 pl-4 text-white outline-none" style="background-color: #3D3D3D; font-family: Poppins;" placeholder="E-MAIL">
+            <input type="email" name='email' class="rounded-full lg:px-3 px-1 py-2 pl-4 text-white outline-none" style="background-color: #3D3D3D; font-family: Poppins;" placeholder="E-MAIL">
           </div>
           <div>
               <p style="font-family: Laro;" class="ml-2 mb-2 text-white">WACHTWOORD:</p>
               <div class="flex">
-              <input type="password" id="myInput" class="rounded-full lg:px-3 px-1 py-2 pl-4 text-white outline-none" style="background-color: #3D3D3D; font-family: Poppins;" placeholder="WACHTWOORD">
+              <input type="password" name='password' id="myInput" class="rounded-full lg:px-3 px-1 py-2 pl-4 text-white outline-none" style="background-color: #3D3D3D; font-family: Poppins;" placeholder="WACHTWOORD">
               <span class='mt-3 ml-4 cursor-pointer hover:scale-110 cursor-pointer ease-in-out duration-300' id="myInput" onclick="myFunction()"><img class='w-5' id='eye' src='icons/ww.png'></span>
             </div>
           </div>
           <div class="mt-2">
-            <p style="font-family: Laro;" class="ml-2 text-white">LEERJAAR:</p>
-            <select class="rounded-full lg:px-3 px-1 py-2 text-white mt-3 pl-4 outline-none" style="background-color: #3D3D3D; font-family: Poppins;">
-              <option value="1">LEERJAAR 1</option>
-              <option value="2">LEERJAAR 2</option>
-              <option value="3">LEERJAAR 3</option>
+            <p style="font-family: Laro;" class="ml-2 text-white">Leerjaar</p>
+            <select name='school_year' class="rounded-full lg:px-3 px-1 py-2 text-white mt-3 pl-4 outline-none" style="background-color: #3D3D3D; font-family: Poppins;">
+              <option value="1">Leerjaar 1</option>
+              <option value="2">Leerjaar 2</option>
+              <option value="3">Leerjaar 3</option>
             </select>
           </div>
         </div>
-        <button class="rounded-full bg-neutral-700 hover:bg-neutral-500 px-20 py-2 mt-16 mb-24 text-white ease-in-out duration-300" style="font-family: Laro;">SUBMIT</button>
+        <button type='submit' name='submit' class="rounded-full bg-neutral-700 hover:bg-neutral-500 px-20 py-2 mt-16 mb-24 text-white ease-in-out duration-300" style="font-family: Laro;">SUBMIT</button>
       </form>
       <div class="flex justify-center">
-        <p class="mb-6 absolute lg:bottom-0 text-white cursor-default pb-6 lg:text-base text-sm" style="font-family: Laro;">HEB JE AL EEN ACCOUNT? <a href="" class="text-sky-500 hover:text-sky-700 ease-in-out duration-300" style="font-family: Laro;">LOGIN</a></p>
+        <p class="mb-6 absolute lg:bottom-0 text-white cursor-default pb-6 lg:text-base text-sm" style="font-family: Laro;">HEB JE AL EEN ACCOUNT? <a href="login.php" class="text-sky-500 hover:text-sky-700 ease-in-out duration-300" style="font-family: Laro;">LOGIN</a></p>
       </div>
 </body>
 </html>
@@ -95,7 +95,7 @@ if (isset($_POST['submit'])) {
   $email = $_POST['email'];
   $first_name = $_POST['first_name'];
   $last_name = $_POST['last_name'];
-  $leerjaar = $_POST['leerjaar'];
+  $school_year = $_POST['school_year'];
   $password = $_POST['password'];
   $sql = "SELECT COUNT(email) AS num FROM users WHERE email = :email";
   $stmt = $pdo->prepare($sql);
@@ -114,11 +114,11 @@ if (isset($_POST['submit'])) {
   if ($row['num'] > 0) {
     echo '<script>alert("Email bestaat al!")</script>';
   } else {
-    $sql = "INSERT INTO `users` (`first_name`, `last_name`, `leerjaar`, `email`, `password`) 
-    VALUES (:first_name, :last_name, :leerjaar, :email, :password)";
+    $sql = "INSERT INTO `users` (`first_name`, `last_name`, `school_year`, `email`, `password`) 
+    VALUES (:first_name, :last_name, :school_year, :email, :password)";
     $sql_run = $pdo->prepare($sql);
     $result = $sql_run->execute(array(
-      ":first_name" => $first_name, ":last_name" => $last_name, ":leerjaar" => $leerjaar, ":email" => $email, ":password" => $password,
+      ":first_name" => $first_name, ":last_name" => $last_name, ":school_year" => $school_year, ":email" => $email, ":password" => $password,
     ));
     header("Location: login.php");
   }
